481a8820e08d90a31729268c0a552784c5d831d0
maximilianjones <maxcharles500@gmail.com>
2021-12-01 10:42:00 -0800

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    makeTodo(e.target.newTask.value)
+  })
 });
+
+function makeTodo(todo) {
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'DONE'
+  li.textContent = `${todo}  `
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+  
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
